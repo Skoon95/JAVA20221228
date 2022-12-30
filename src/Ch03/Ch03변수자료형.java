e09048a31136a54974507199f299031d127f7b0e
Skoon95 <tjrgns21@naver.com>
2022-12-30 18:40:04 +0900

    javacalss

---
+++ src/Ch03/Ch03변수자료형.java
@@ -0,0 +1,18 @@
+package Ch03;
+
+public class Ch03변수자료형 {
+
+	public static void main(String[] args) {
+		
+		//LeftValue=RightValue  -대입연산자(=)를 기준으로 왼쪽은 lv(공간) 오른쪽은 rv(값)
+		//lv(공간)=rv(값)
+		
+		int num1;
+		num1=10;
+		int num2=4;
+		int num3=num1+num2;
+		System.out.println(num3);
+
+	}
+
+}
