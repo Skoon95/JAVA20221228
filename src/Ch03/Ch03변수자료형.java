71f7c2377863f5e5f9e09199ac926892805e0d56
Skoon95 <tjrgns21@naver.com>
2022-12-30 18:47:14 +0900

    java day 1230

--- src/Ch03/Ch03변수자료형.java
+++ src/Ch03/Ch03변수자료형.java
@@ -9,9 +9,9 @@ public class Ch03변수자료형 {
 		
 		int num1; //4byte정구 공간 생성 +num1 이름 부여 (변수 선언 or 정의)
 		num1=10;  //10값을 상수Pool에 저장하고 num1공간에 복사(대입)
-		int num2=4;
-		int num3=num1+num2;
-		System.out.println(num3);
+		int num2=4; // 4값을 상수Pool에 저장(int)하고 새로만들어지는 4byte정수공간 num2에 초기화
+		int num3=num1+num2; //num1의 값+ num2의 값을 4byte정수공간 num3에 초기화
+		System.out.println(num3); // num3의 값을 print
 
 	}
 
