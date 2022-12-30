3f262ac8b25f29f6addad292c987539082e1196f
Skoon95 <tjrgns21@naver.com>
2022-12-30 18:43:38 +0900

    javaclass

--- src/Ch03/Ch03변수자료형.java
+++ src/Ch03/Ch03변수자료형.java
@@ -7,8 +7,8 @@ public class Ch03변수자료형 {
 		//LeftValue=RightValue  -대입연산자(=)를 기준으로 왼쪽은 lv(공간) 오른쪽은 rv(값)
 		//lv(공간)=rv(값)
 		
-		int num1;
-		num1=10;
+		int num1; //4byte정구 공간 생성 +num1 이름 부여 (변수 선언 or 정의)
+		num1=10;  //10값을 상수Pool에 저장하고 num1공간에 복사(대입)
 		int num2=4;
 		int num3=num1+num2;
 		System.out.println(num3);
