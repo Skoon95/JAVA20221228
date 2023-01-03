7d29fe8d2d476e5264ce98718ad90e5914b4905c
Skoon95 <tjrgns21@naver.com>
2023-01-03 18:39:56 +0900

    Java Day 0103

---
+++ src/Ch06/C01Scarnner.java
@@ -0,0 +1,22 @@
+package Ch06;
+
+import java.util.Scanner;
+
+public class C01Scarnner {
+
+	public static void main(String[] args) {
+		
+		//import 없을 떄 Crtl +Shift + O
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 =sc.nextInt();
+		int num2 =sc.nextInt();
+		int num3 =sc.nextInt();
+		int num4 =sc.nextInt();
+		int sum = num1 + num2 + num3+ num4;
+		System.out.printf("%d+ %d+ %d+ %d = %d \n", num1,num2,num3,sum);
+		sc.close();
+
+	}
+
+}
