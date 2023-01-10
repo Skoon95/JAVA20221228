73cb15e1f67b71b86619e71ad8b4e99866593d55
Skoon95 <tjrgns21@naver.com>
2023-01-10 18:29:54 +0900

    Java Day 01 10

---
+++ src/Ch09/C04For.java
@@ -0,0 +1,29 @@
+package Ch09;
+
+public class C04For {
+
+	public static void main(String[] args) {
+		// 
+//		for(int i=0;i<10;i++)
+//		{
+//			System.out.println("Hello World");
+//		}
+		
+		//1부터 10까지의 합
+		for(int i=0;i<=10;i++)
+		{
+			
+		}
+		
+		//1부터 N까지의 합
+		//N부터 M까지의 합
+		//2단 출력
+		//N단 출력
+		//2~9단 출력
+		//2~N단 출력
+		//N~M단 출력
+		//별찍기~
+
+	}
+
+}
