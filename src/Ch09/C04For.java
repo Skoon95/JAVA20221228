fce11f65b988339eaed2daf2f2fc286e9ec2e4b6
Skoon95 <tjrgns21@naver.com>
2023-01-26 18:50:35 +0900

    Java Day 23 01 26

--- src/Ch09/C04For.java
+++ src/Ch09/C04For.java
@@ -1,8 +1,11 @@
 package Ch09;
 
+import java.util.Scanner;
+
 public class C04For {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		// 
 //		for(int i=0;i<10;i++)
 //		{
---
+++ src/Ch22/C02DBconn.java
@@ -0,0 +1,38 @@
+package Ch22;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class C02DBconn {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 연결관련 정보 저장용 변수
+		String id = "root";
+		String pw = "1234";
+		String url = "jdbc:mysql://localhost/testDB"; //연결URL (DBMS마다 상이함)
+		
+		//DB연결객체 관련 참조변수
+		Connection conn = null;         //DB연결객체용 참조변수
+		PreparedStatement pstmt = null; //SQL쿼리 전송객체용 참조변수
+		ResultSet rs = null;
+		
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");  //1 드라이브 메모리 적재
+		System.out.println("Driver Loading Success!!");
+		conn = DriverManager.getConnection(url,id,pw); //2 DB 연결
+		System.out.println("DB Connected.....");
+		
+		
+		
+		
+		
+		
+		
+	
+	}
+
+}
